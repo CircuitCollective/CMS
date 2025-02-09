967b60f4a45b4690577041f6892047a2bc1c5668
Tobenna Nnaobi <tobenna.nnaobi@ontariotechu.net>
2025-02-08 21:12:42 -0500

    Added Nathan

--- web/front_end/js/load_file.js
+++ web/front_end/js/load_file.js
@@ -1,3 +1,5 @@
+const url = "http://localhost:8080"
+
 let inputFile_Location = document.getElementById("desired_file_upload")
 let chooseFile_Button = document.getElementById("choose_desired_file")
 let textFile_Name = document.getElementById("text_file_name")
@@ -10,56 +12,59 @@ inputFile_Location.addEventListener("change", function (){
 })
 
 function import_csv_data() {
-    const csvFile = inputFile_Location.files[0]
-    Papa.parse(csvFile, {
-        header: true,
-        complete: function (results) {
-            load_csv_data(results.data)
-            console.log(results)
-        },
-    })
+    inputFile_Location.addEventListener("change", () =>
+            fetch(`${url}/dummy/batch`, {
+                method: "POST",
+                body: new FormData(document.getElementById("form"))
+            }).then(load_csv_data))
 }
 
-function load_csv_data(csvData) {
-    let userRow_Table = document.getElementById("user_data")
+function load_csv_data() {
+    fetch(`${url}/dummy`).then(c => c.json()).then(render_csv_data)
+    function render_csv_data(csvData) {
+        let userRow_Table = document.getElementById("user_data")
+        for (const row_data of csvData) {
+            let loadUser_Data = document.createElement("tr")
 
-    for (const row_data of csvData) {
-        let loadUser_Data = document.createElement("tr")
+            let loadedUser_ID = document.createElement("td")
+            loadedUser_ID.innerHTML = row_data.id
+            loadUser_Data.appendChild(loadedUser_ID)
 
-        let loadedUser_ID = document.createElement("td")
-        loadedUser_ID.innerHTML = row_data.id
-        loadUser_Data.appendChild(loadedUser_ID)
+            let loadedUser_Name = document.createElement("td")
+            loadedUser_Name.innerHTML = row_data.name
+            loadUser_Data.appendChild(loadedUser_Name)
 
-        let loadedUser_Name = document.createElement("td")
-        loadedUser_Name.innerHTML = row_data.name
-        loadUser_Data.appendChild(loadedUser_Name)
+            let loadedUser_Program = document.createElement("td")
+            loadedUser_Program.innerHTML = row_data.program
+            loadUser_Data.appendChild(loadedUser_Program)
 
-        let loadedUser_Program = document.createElement("td")
-        loadedUser_Program.innerHTML = row_data.program
-        loadUser_Data.appendChild(loadedUser_Program)
-
-        let loadedUser_Faculty = document.createElement("td")
-        loadedUser_Faculty.innerHTML = row_data.faculty
-        loadUser_Data.appendChild(loadedUser_Faculty)
+            let loadedUser_Faculty = document.createElement("td")
+            loadedUser_Faculty.innerHTML = row_data.faculty
+            loadUser_Data.appendChild(loadedUser_Faculty)
 
 
-        const editUserButton = document.createElement("td")
-        let editUser_OnClick = document.createElement("button")
-        editUser_OnClick.type = "button"
-        editUser_OnClick.textContent = "Edit User"
-        editUser_OnClick.addEventListener("click", function(){edit_user_data(this)})
-        editUserButton.appendChild(editUser_OnClick)
-        loadUser_Data.appendChild(editUserButton)
+            const editUserButton = document.createElement("td")
+            let editUser_OnClick = document.createElement("button")
+            editUser_OnClick.type = "button"
+            editUser_OnClick.textContent = "Edit User"
+            editUser_OnClick.addEventListener("click", function () {
+                edit_user_data(this)
+            })
+            editUserButton.appendChild(editUser_OnClick)
+            loadUser_Data.appendChild(editUserButton)
 
-        const removeUserButton = document.createElement("td")
-        let removeUser_OnClick = document.createElement("button")
-        removeUser_OnClick.type = "button"
-        removeUser_OnClick.textContent = "Remove User"
-        removeUser_OnClick.addEventListener("click", function(){remove_row(parseInt(row_data.id))})
-        removeUserButton.appendChild(removeUser_OnClick)
-        loadUser_Data.appendChild(removeUserButton)
+            const removeUserButton = document.createElement("td")
+            let removeUser_OnClick = document.createElement("button")
+            removeUser_OnClick.type = "button"
+            removeUser_OnClick.textContent = "Remove User"
+            removeUser_OnClick.addEventListener("click", function () {
+                remove_row(parseInt(row_data.id))
+            })
+            removeUserButton.appendChild(removeUser_OnClick)
+            loadUser_Data.appendChild(removeUserButton)
 
-        userRow_Table.appendChild(loadUser_Data)
+            userRow_Table.appendChild(loadUser_Data)
+        }
     }
 }
 
