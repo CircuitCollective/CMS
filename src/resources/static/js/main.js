51365093f9fe2fb6b1cf84937955323ae74a3e24
buthed010203 <naguiar010203@gmail.com>
2025-03-08 17:52:50 -0500

    Refactor frontend. (#33)
    * Refactor frontend.
    
    * Fix row replacement
    
    * Replace user -> game.
    
    * Implement CSV parser.
    
    * Implement search bar

--- src/resources/static/js/main.js
+++ src/resources/static/js/main.js
@@ -1,253 +1,187 @@
-let initialized_row_value = 0
-let url_backend = "http://localhost:8080"
+const api = "http://localhost:8080/api"
+const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1');
 
-function create_user_row() {
-    const generated_id = Math.floor(Math.random() * 1000) + 1 //variable to store randomly generated id
+const inputs = ["name", "desc", "stock", "tags"] // Maintained list of input values
+const listInputs = new Set(["tags"]) // Inputs with potentially many values
 
-    const input_name = document.getElementById("name")
-    const value_name = input_name.value
+function init() {
+    const inputTable = document.getElementById("game_creation_inputs")
+    const inputHead = document.getElementById("game_creation_head")
+    const itemsRow = document.getElementById("items_row")
 
-    const input_program = document.getElementById("program")
-    const value_program = input_program.value
-
-    const input_faculty = document.getElementById("faculty")
-    const value_faculty = input_faculty.value
-
-    if (value_name === "" || value_program === "" || value_faculty === "") {
-        alert("The input fields cannot be empty!")
-        return;
+    itemsRow.innerHTML += '<th>id</th>'
+    for (const input of inputs) {
+        inputHead.innerHTML += `<th>${input}</th>`
+        inputTable.innerHTML += `<td><input type="text" id="${input}" placeholder="${input}"></td>`
+        itemsRow.innerHTML += `<th>${input}</th>`
     }
 
-    initialized_row_value++
-    let userData = document.getElementById("user_data")
-    let userRow = document.createElement("tr")
-    userRow.id = String(initialized_row_value)
+    inputTable.innerHTML += '<td><button id="create_game" onclick="create_game_row()">Create Game</button></td>'
 
-    let userID  = document.createElement("td")
-    userID.innerHTML = generated_id
-    userRow.appendChild(userID)
+    // CSV Parsing.
+    const file = document.getElementById("file")
+    file.addEventListener("change", () =>
+        fetch(`${api}/admin/game/batch`, { method: "POST", body: new FormData(document.getElementById("form")) }).then(refresh))
 
-    let userName = document.createElement("td")
-    userName.innerHTML = value_name
-    userRow.appendChild(userName)
+    // Search Bar
+    const searchInput = document.getElementById("search_bar")
+    searchInput.addEventListener("keydown", e => { // On enter: update search
+        if (e.key !== "Enter") return
 
-    let userProgram  = document.createElement("td")
-    userProgram.innerHTML = value_program
-    userRow.appendChild(userProgram)
+        const query = searchInput.value
+        if (query.trim() === "") refresh() // Blank: do full refresh
+        else { // Otherwise: load search results
+            clearTable()
+            fetch(`${api}/game/search?l=25&q=${query}`)
+                .then(response => response.json())
+                .then(response => response.forEach(r => upsertRow(r)))
+                .catch(error => console.error(error))
+        }
+    })
+}
 
-    let userFaculty  = document.createElement("td")
-    userFaculty.innerHTML = value_faculty
-    userRow.appendChild(userFaculty)
+function create_game_row() {
+    const obj = Object.fromEntries(inputs.map(input => [input, document.getElementById(input).value]))
+    upsert_game(obj)
+}
 
-    let editUserButton = document.createElement("td")
-    let editUser_OnClick = document.createElement("button")
-    editUser_OnClick.type = "button"
-    editUser_OnClick.textContent = "Edit User"
-    editUser_OnClick.addEventListener("click", function(){edit_user_menu(this, parseInt(userRow.id))})
-    editUserButton.appendChild(editUser_OnClick)
-    userRow.appendChild(editUserButton)
+/** Inserts or updates a game */
+function upsert_game(obj, id = undefined) {
+    for (let input in obj) {
+        let value = obj[input]
 
-    let removeUserButton = document.createElement("td")
-    let removeUser_OnClick = document.createElement("button")
-    removeUser_OnClick.type = "button"
-    removeUser_OnClick.textContent = "Remove User"
-    removeUser_OnClick.addEventListener("click", function(){remove_row(parseInt(userRow.id))})
-    removeUserButton.appendChild(removeUser_OnClick)
-    userRow.appendChild(removeUserButton)
+        // List inputs are split into a list with trimming applied
+        if (listInputs.has(input)) obj[input] = value = value.split(",").map(v => v.trim())
+        // Blank values are bad
+        if (value == null || (!Array.isArray(value) && typeof value !== "number" && value.trim() === '' || Array.isArray(value) && value.some(v => v.trim() === ''))) {
+            alert(`The ${input} field cannot be empty!`)
+            return
+        }
+    }
 
-    fetch(`${url_backend}/game/${generated_id}`, {
+    console.log(obj)
+
+    fetch(`${api}/admin/game/${id ?? -1}`, {
         method: "PUT",
-        body: JSON.stringify({
-            "id" : generated_id,
-            "name": value_name,
-            "program": value_program,
-            "faculty": value_faculty,
-        }),
+        body: JSON.stringify(obj),
         headers: {
             "Content-Type": "application/json",
+            'X-XSRF-TOKEN': csrfToken
         }
-    }).then(obtain_database_data)
-        .catch(error => {console.log(error.message)})
-
-    userData.appendChild(userRow)
-
-
-    input_name.value = ""
-    input_program.value = ""
-    input_faculty.value = ""
+    }).then(r => {
+        if (r.ok) r.json().then(r => upsertRow(r, id))
+        else if (r.status === 400) r.json().then(r => alert(r.message)) // TODO: Parse the errors to be prettier?
+        else r.json().then(t => { console.error(r); console.error(t) })
+    })
 }
 
 function remove_row(row_value) {
-    let userData_Table = document.getElementById("user_data")
-    let userRow_Data = document.getElementById(row_value)
+    let gameData_Table = document.getElementById("game_data")
+    let gameRow_Data = document.getElementById(row_value)
     let editing_data_row = document.getElementById("editing_row")
 
-    if (userData_Table.rows.length > 0) {
-        userData_Table.removeChild(userRow_Data)
-        userData_Table.removeChild(editing_data_row)
-    }
+    fetch(`${api}/admin/game/${row_value}`, { method: "DELETE", headers: { 'X-XSRF-TOKEN': csrfToken } }).then(() => {
+        if (gameData_Table.rows.length > 0) {
+            gameData_Table.removeChild(gameRow_Data)
+            if (editing_data_row) gameData_Table.removeChild(editing_data_row)
+        }
+    }).catch(error => console.error(error))
 }
 
+function edit_game_menu(edit_game_button, gameId) {
+    const currentRow = document.getElementById(gameId)
 
+    const editInputs = document.createElement("tr")
+    editInputs.id = "editing_row"
 
-function edit_user_menu(edit_user_button, user_row_id) {
-    let get_current_row = document.getElementById(user_row_id)
+    editInputs.innerHTML += `<td>${gameId}</td>`
+    for (const input of inputs) {
+        editInputs.innerHTML += `<td><input type="text" id="${input}-${gameId}" placeholder="${input}"></td>`
+    }
 
-    let edit_row_inputs = document.createElement("tr")
-    edit_row_inputs.id = "editing_row"
+    const saveGameButton = document.createElement("td")
+    const saveGame_OnClick = document.createElement("button")
+    saveGame_OnClick.type = "button"
+    saveGame_OnClick.textContent = "Save New Game"
+    saveGame_OnClick.onclick = () => {
+        upsert_game(Object.fromEntries(inputs.map(input => [input, document.getElementById(`${input}-${gameId}`).value])), gameId)
+        cancel_row_edit(editInputs.id)
+    }
+    saveGameButton.appendChild(saveGame_OnClick)
+    editInputs.appendChild(saveGameButton)
 
-
-    const newID_input = document.createElement("td")
-    const edit_id = document.createElement("input")
-    edit_id.type = "text"
-    edit_id.id = "new_id"
-    edit_id.placeholder = "New ID"
-    newID_input.appendChild(edit_id)
-    edit_row_inputs.appendChild(newID_input)
-
-    const newName_input = document.createElement("td")
-    const edit_name = document.createElement("input")
-    edit_name.type = "text"
-    edit_name.id = "new_name"
-    edit_name.placeholder = "New Name"
-    newName_input.appendChild(edit_name)
-    edit_row_inputs.appendChild(newName_input)
-
-    const newProgram_input = document.createElement("td")
-    const edit_program = document.createElement("input")
-    edit_program.type = "text"
-    edit_program.id = "new_program"
-    edit_program.placeholder = "New Program"
-    newProgram_input.appendChild(edit_program)
-    edit_row_inputs.appendChild(newProgram_input)
-
-    const newFaculty_input = document.createElement("td")
-    const edit_faculty = document.createElement("input")
-    edit_faculty.type = "text"
-    edit_faculty.id = "new_faculty"
-    edit_faculty.placeholder = "New Faculty"
-    newFaculty_input.appendChild(edit_faculty)
-    edit_row_inputs.appendChild(newFaculty_input)
-
-    const saveUserButton = document.createElement("td")
-    const saveUser_OnClick = document.createElement("button")
-    saveUser_OnClick.type = "button"
-    saveUser_OnClick.textContent = "Save New User"
-    saveUser_OnClick.addEventListener("click", function(){
-        save_edited_user(edit_user_button,
-            edit_row_inputs.id,
-            edit_id,
-            edit_name,
-            edit_program,
-            edit_faculty)
-    })
-    saveUserButton.appendChild(saveUser_OnClick)
-    edit_row_inputs.appendChild(saveUserButton)
-
+    // TODO: Rewrite
     const cancelEditButton = document.createElement("td")
     const cancelEdit_OnClick = document.createElement("button")
     cancelEdit_OnClick.type = "button"
     cancelEdit_OnClick.textContent = "Cancel"
     cancelEdit_OnClick.addEventListener("click", function(){
-        cancel_row_edit(edit_row_inputs.id)
+        cancel_row_edit(editInputs.id)
     })
     cancelEditButton.appendChild(cancelEdit_OnClick)
-    edit_row_inputs.appendChild(cancelEditButton)
+    editInputs.appendChild(cancelEditButton)
 
-    $(edit_user_button).ready(function() {
-        $(edit_row_inputs).insertAfter(get_current_row)
+    $(edit_game_button).ready(function() { // TODO: This is the sole place we use jQuery. We should either use it more or remove it entirely.
+        $(editInputs).insertAfter(currentRow)
     })
-}
-
-function save_edited_user(edit_user_button, row_value, edited_id, edited_name, edited_program, edited_faculty) {
-    if (edited_id.value === "" || edited_name.value === "" || edited_program.value === "" || edited_faculty.value === "") {
-        alert("The input fields cannot be empty!")
-        return
-    }
-
-    let userData_Table = document.getElementById("user_data")
-    let userRow_Data = document.getElementById(row_value)
-    let edit_user_row = edit_user_button.parentNode.parentNode
-
-    let newID_Data = edit_user_row.cells[0]
-    let newName_Data = edit_user_row.cells[1]
-    let newProgram_Data = edit_user_row.cells[2]
-    let newFaculty_Data = edit_user_row.cells[3]
-
-    newID_Data.innerHTML = edited_id.value
-    newName_Data.innerHTML = edited_name.value
-    newProgram_Data.innerHTML = edited_program.value
-    newFaculty_Data.innerHTML = edited_faculty.value
-
-    edited_id.value = ""
-    edited_name.value = ""
-    edited_program.value = ""
-    edited_faculty.value = ""
-
-    if (userData_Table.rows.length > 0) {
-        userData_Table.removeChild(userRow_Data)
-    }
 }
 
 function cancel_row_edit(row_value) {
-    let userData_Table = document.getElementById("user_data")
-    let userRow_Data = document.getElementById(row_value)
+    let gameData_Table = document.getElementById("game_data")
+    let gameRow_Data = document.getElementById(row_value)
 
-    if (userData_Table.rows.length > 0) {
-        userData_Table.removeChild(userRow_Data)
+    if (gameData_Table.rows.length > 0) {
+        gameData_Table.removeChild(gameRow_Data)
     }
 }
 
+/** Adds a row with a game from json */
+function upsertRow(game_data, id = undefined) { // TODO: Remove the id param on rewrite
+    const gameRow_Table = document.getElementById("game_data")
+    const row = document.createElement("tr")
+    for (const col in game_data) {
+        let td = document.createElement("td")
+        td.textContent = game_data[col]
+        row.appendChild(td)
+    }
 
-function obtain_database_data() {
-    fetch(`${url_backend}/game/}`)
+    // TODO: Rewrite
+    const editGameButton = document.createElement("td")
+    let editGame_OnClick = document.createElement("button")
+    editGame_OnClick.type = "button"
+    editGame_OnClick.textContent = "Edit Game"
+    editGame_OnClick.onclick = () => edit_game_menu(this, game_data.id)
+    editGameButton.appendChild(editGame_OnClick)
+    row.appendChild(editGameButton)
+
+    const removeGameButton = document.createElement("td")
+    let removeGame_OnClick = document.createElement("button")
+    removeGame_OnClick.type = "button"
+    removeGame_OnClick.textContent = "Remove Game"
+    removeGame_OnClick.addEventListener("click", function(){remove_row(parseInt(game_data.id))})
+    removeGameButton.appendChild(removeGame_OnClick)
+    row.appendChild(removeGameButton)
+
+    row.id = game_data.id
+    if (typeof id !== "undefined") document.getElementById(id).replaceWith(row) // Update row
+    else gameRow_Table.appendChild(row) // Insert row
+}
+
+/** Refreshes the table */
+function refresh() {
+    clearTable()
+    fetch(`${api}/game`)
         .then(response => response.json())
-        .then(load_database_data)
-        .catch(error => {console.log(error.message)})
-    function load_database_data(database_data) {
-        let userRow_Table = document.getElementById("user_data")
-
-        for (const row_data of database_data) {
-            let loadUser_Data = document.createElement("tr")
-
-            let loadedUser_ID = document.createElement("td")
-            loadedUser_ID.innerHTML = row_data.id
-            loadUser_Data.appendChild(loadedUser_ID)
-
-            let loadedUser_Name = document.createElement("td")
-            loadedUser_Name.innerHTML = row_data.name
-            loadUser_Data.appendChild(loadedUser_Name)
-
-            let loadedUser_Program = document.createElement("td")
-            loadedUser_Program.innerHTML = row_data.program
-            loadUser_Data.appendChild(loadedUser_Program)
-
-            let loadedUser_Faculty = document.createElement("td")
-            loadedUser_Faculty.innerHTML = row_data.faculty
-            loadUser_Data.appendChild(loadedUser_Faculty)
-
-
-            const editUserButton = document.createElement("td")
-            let editUser_OnClick = document.createElement("button")
-            editUser_OnClick.type = "button"
-            editUser_OnClick.textContent = "Edit User"
-            editUser_OnClick.addEventListener("click", function(){edit_user_data(this)})
-            editUserButton.appendChild(editUser_OnClick)
-            loadUser_Data.appendChild(editUserButton)
-
-            const removeUserButton = document.createElement("td")
-            let removeUser_OnClick = document.createElement("button")
-            removeUser_OnClick.type = "button"
-            removeUser_OnClick.textContent = "Remove User"
-            removeUser_OnClick.addEventListener("click", function(){remove_row(parseInt(row_data.id))})
-            removeUserButton.appendChild(removeUser_OnClick)
-            loadUser_Data.appendChild(removeUserButton)
-
-            userRow_Table.appendChild(loadUser_Data)
-        }
-    }
+        .then(response => response.forEach(r => upsertRow(r)))
+        .catch(error => console.error(error))
 }
 
-(function() {
-    obtain_database_data()
-})()
+/** Clears the table */
+function clearTable() {
+    document.getElementById("game_data").innerHTML = "" // Clear the existing game list
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    refresh()
+    init()
+}, false);
