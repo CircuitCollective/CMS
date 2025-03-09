ed5153abbe1c71d6de9dfac494db6350e6d59b24
buthed010203 <naguiar010203@gmail.com>
2025-03-08 19:46:17 -0500

    Many changes (expand commit for details). - Use custom login page now that its working - Add login header to index - Add thymeleaf templating capabilities - More work towards a concise, well documented frontend - When editing an item, fill in existing values

--- src/resources/static/js/main.js
+++ src/resources/static/js/main.js
@@ -4,7 +4,9 @@ const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;
 const inputs = ["name", "desc", "stock", "tags"] // Maintained list of input values
 const listInputs = new Set(["tags"]) // Inputs with potentially many values
 
+/** Function that runs on page load. Sets up the game list, search, and csv parsing behavior. */
 function init() {
+    // Game list
     const inputTable = document.getElementById("game_creation_inputs")
     const inputHead = document.getElementById("game_creation_head")
     const itemsRow = document.getElementById("items_row")
@@ -16,12 +18,12 @@ function init() {
         itemsRow.innerHTML += `<th>${input}</th>`
     }
 
-    inputTable.innerHTML += '<td><button id="create_game" onclick="create_game_row()">Create Game</button></td>'
+    inputTable.innerHTML += '<td><button id="create_game" onclick="upsertGame(Object.fromEntries(inputs.map(input => [input, document.getElementById(input).value])))">Create Game</button></td>'
 
-    // CSV Parsing.
+    // CSV Parsing
     const file = document.getElementById("file")
     file.addEventListener("change", () =>
-        fetch(`${api}/admin/game/batch`, { method: "POST", body: new FormData(document.getElementById("form")) }).then(refresh))
+        fetch(`${api}/admin/game/batch`, { method: "POST", body: new FormData(document.getElementById("form")) }).then(r => r.ok ? refresh() : r.json().then(console.error)))
 
     // Search Bar
     const searchInput = document.getElementById("search_bar")
@@ -31,27 +33,22 @@ function init() {
         const query = searchInput.value
         if (query.trim() === "") refresh() // Blank: do full refresh
         else { // Otherwise: load search results
-            clearTable()
+            clearGameList()
             fetch(`${api}/game/search?l=25&q=${query}`)
                 .then(response => response.json())
-                .then(response => response.forEach(r => upsertRow(r)))
+                .then(response => response.forEach(upsertRow))
                 .catch(error => console.error(error))
         }
     })
 }
 
-function create_game_row() {
-    const obj = Object.fromEntries(inputs.map(input => [input, document.getElementById(input).value]))
-    upsert_game(obj)
-}
-
-/** Inserts or updates a game */
-function upsert_game(obj, id = undefined) {
-    for (let input in obj) {
-        let value = obj[input]
+/** Inserts or updates a game from given {@link json}. This validates the inputs and then saves to the database. Use {@link upsertRow} if you wish to update a row directly. */
+function upsertGame(json, id = undefined) {
+    for (let input in json) {
+        let value = json[input]
 
         // List inputs are split into a list with trimming applied
-        if (listInputs.has(input)) obj[input] = value = value.split(",").map(v => v.trim())
+        if (listInputs.has(input)) json[input] = value = value.split(",").map(v => v.trim())
         // Blank values are bad
         if (value == null || (!Array.isArray(value) && typeof value !== "number" && value.trim() === '' || Array.isArray(value) && value.some(v => v.trim() === ''))) {
             alert(`The ${input} field cannot be empty!`)
@@ -59,53 +56,72 @@ function upsert_game(obj, id = undefined) {
         }
     }
 
-    console.log(obj)
-
     fetch(`${api}/admin/game/${id ?? -1}`, {
         method: "PUT",
-        body: JSON.stringify(obj),
+        body: JSON.stringify(json),
         headers: {
             "Content-Type": "application/json",
             'X-XSRF-TOKEN': csrfToken
         }
     }).then(r => {
-        if (r.ok) r.json().then(r => upsertRow(r, id))
+        if (r.ok) r.json().then(upsertRow)
         else if (r.status === 400) r.json().then(r => alert(r.message)) // TODO: Parse the errors to be prettier?
         else r.json().then(t => { console.error(r); console.error(t) })
     })
 }
 
-function remove_row(row_value) {
-    let gameData_Table = document.getElementById("game_data")
-    let gameRow_Data = document.getElementById(row_value)
-    let editing_data_row = document.getElementById("editing_row")
+/** Adds a row with a game from json */
+function upsertRow(gameData) {
+    const row = document.createElement("tr")
+    row.innerHTML += `<td>${gameData.id}</td>`
+    for (const col of inputs) {
+        let td = document.createElement("td")
+        td.textContent = gameData[col]
+        row.appendChild(td)
+    }
 
-    fetch(`${api}/admin/game/${row_value}`, { method: "DELETE", headers: { 'X-XSRF-TOKEN': csrfToken } }).then(() => {
-        if (gameData_Table.rows.length > 0) {
-            gameData_Table.removeChild(gameRow_Data)
-            if (editing_data_row) gameData_Table.removeChild(editing_data_row)
+    row.innerHTML += `<td><button type="button" onclick="startGameEdit(this, ${gameData.id})">Edit Game</button>` // Edit button
+    row.innerHTML += `<td><button type="button" onclick="deleteGame(${gameData.id})">Remove Game</button>`// Delete button
+
+    row.id = gameData.id
+    // Update the row if it exists, create it otherwise
+    const existingRow = document.getElementById(gameData.id)
+    existingRow ? existingRow.replaceWith(row) : document.getElementById("game_data").appendChild(row)
+}
+
+/** Deletes an existing game from the database */
+function deleteGame(id) {
+    const gameRow = document.getElementById(id)
+
+    fetch(`${api}/admin/game/${id}`, { method: "DELETE", headers: { 'X-XSRF-TOKEN': csrfToken } }).then(r => {
+        const gameList = document.getElementById("game_data")
+        if (r.ok && gameList.rows.length > 0) {
+            if (gameRow) document.getElementById("game_data").removeChild(gameRow)
+            stopGameEdit()
         }
     }).catch(error => console.error(error))
 }
 
-function edit_game_menu(edit_game_button, gameId) {
+/** Begins the edit of a game by inserting a row below the current one */
+function startGameEdit(edit_game_button, gameId) {
     const currentRow = document.getElementById(gameId)
 
     const editInputs = document.createElement("tr")
     editInputs.id = "editing_row"
 
     editInputs.innerHTML += `<td>${gameId}</td>`
-    for (const input of inputs) {
-        editInputs.innerHTML += `<td><input type="text" id="${input}-${gameId}" placeholder="${input}"></td>`
-    }
+    let colI = 1
+    for (const input of inputs)
+        editInputs.innerHTML += `<td><input type="text" id="${input}-${gameId}" placeholder="${input}" value="${currentRow.children.item(colI++).textContent}"></td>`
 
+    // TODO: Rewrite
     const saveGameButton = document.createElement("td")
     const saveGame_OnClick = document.createElement("button")
     saveGame_OnClick.type = "button"
     saveGame_OnClick.textContent = "Save New Game"
     saveGame_OnClick.onclick = () => {
-        upsert_game(Object.fromEntries(inputs.map(input => [input, document.getElementById(`${input}-${gameId}`).value])), gameId)
-        cancel_row_edit(editInputs.id)
+        upsertGame(Object.fromEntries(inputs.map(input => [input, document.getElementById(`${input}-${gameId}`).value])), gameId)
+        stopGameEdit()
     }
     saveGameButton.appendChild(saveGame_OnClick)
     editInputs.appendChild(saveGameButton)
@@ -116,7 +132,7 @@ function edit_game_menu(edit_game_button, gameId) {
     cancelEdit_OnClick.type = "button"
     cancelEdit_OnClick.textContent = "Cancel"
     cancelEdit_OnClick.addEventListener("click", function(){
-        cancel_row_edit(editInputs.id)
+        stopGameEdit()
     })
     cancelEditButton.appendChild(cancelEdit_OnClick)
     editInputs.appendChild(cancelEditButton)
@@ -126,62 +142,29 @@ function edit_game_menu(edit_game_button, gameId) {
     })
 }
 
-function cancel_row_edit(row_value) {
-    let gameData_Table = document.getElementById("game_data")
-    let gameRow_Data = document.getElementById(row_value)
+/** Removes the row for edits of the current row */
+function stopGameEdit() {
+    const gameList = document.getElementById("game_data")
+    const gameEditRow = document.getElementById("editing_row")
 
-    if (gameData_Table.rows.length > 0) {
-        gameData_Table.removeChild(gameRow_Data)
-    }
+    if (gameList && gameEditRow && gameList.rows.length > 0) gameList.removeChild(gameEditRow)
 }
 
-/** Adds a row with a game from json */
-function upsertRow(game_data, id = undefined) { // TODO: Remove the id param on rewrite
-    const gameRow_Table = document.getElementById("game_data")
-    const row = document.createElement("tr")
-    for (const col in game_data) {
-        let td = document.createElement("td")
-        td.textContent = game_data[col]
-        row.appendChild(td)
-    }
-
-    // TODO: Rewrite
-    const editGameButton = document.createElement("td")
-    let editGame_OnClick = document.createElement("button")
-    editGame_OnClick.type = "button"
-    editGame_OnClick.textContent = "Edit Game"
-    editGame_OnClick.onclick = () => edit_game_menu(this, game_data.id)
-    editGameButton.appendChild(editGame_OnClick)
-    row.appendChild(editGameButton)
-
-    const removeGameButton = document.createElement("td")
-    let removeGame_OnClick = document.createElement("button")
-    removeGame_OnClick.type = "button"
-    removeGame_OnClick.textContent = "Remove Game"
-    removeGame_OnClick.addEventListener("click", function(){remove_row(parseInt(game_data.id))})
-    removeGameButton.appendChild(removeGame_OnClick)
-    row.appendChild(removeGameButton)
-
-    row.id = game_data.id
-    if (typeof id !== "undefined") document.getElementById(id).replaceWith(row) // Update row
-    else gameRow_Table.appendChild(row) // Insert row
-}
-
-/** Refreshes the table */
+/** Refreshes the game list */
 function refresh() {
-    clearTable()
+    clearGameList()
     fetch(`${api}/game`)
         .then(response => response.json())
-        .then(response => response.forEach(r => upsertRow(r)))
+        .then(response => response.forEach(upsertRow))
         .catch(error => console.error(error))
 }
 
-/** Clears the table */
-function clearTable() {
+/** Clears the game list */
+function clearGameList() {
     document.getElementById("game_data").innerHTML = "" // Clear the existing game list
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // On load
     refresh()
     init()
 }, false);
