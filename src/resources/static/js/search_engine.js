c1458755aafb0daf66cb32e0d48576aa23e8ab3a
Tobenna Nnaobi <tobennannaobi@gmail.com>
2025-03-22 02:37:05 -0400

    Had to make a new branch for the new UI

---
+++ src/resources/static/js/search_engine.js
@@ -0,0 +1,21 @@
+const search_input_menu = document.getElementById("search_bar_input") //Gain access to the search bar
+
+//Implement a addEventListener() "keydown" event that allows the administrator
+//to search for any game they need to view using specific keywords in the string.
+search_input_menu.addEventListener("keydown", function(event_key) {
+    if (event_key.key !== "Enter") {
+        return
+    }
+
+    const desired_games_search = search_input_menu.value
+    if (desired_games_search.trim() === "") {
+        refresh_the_page()
+    }
+    else {
+        clearGame_ListData()
+        fetch(`${api_url}/api/game/search?l-25&q=${desired_games_search}`)
+            .then(response => response.json())
+            .then(response => response.forEach(obtain_database_data))
+            .catch(error => console.error(error.message))
+    }
+})
